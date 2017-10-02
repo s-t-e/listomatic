f85b8ff59f7262f3b63b156b9e630f53c13f1474
s-t-e <s-t-e@users.noreply.github.com>
2017-10-02 21:16:56 +0100

    Simple navigation drawer added.

--- app/src/main/java/s_t_e/listomatic/MainActivity.java
+++ app/src/main/java/s_t_e/listomatic/MainActivity.java
@@ -1,10 +1,17 @@
 package s_t_e.listomatic;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,15 +30,101 @@ public class MainActivity extends AppCompatActivity {
 
     private String[] menuOptions = {"Home", "Browse", "Search", "Favourites"};
 
+    private ActionBarDrawerToggle drawerToggle;
+    private CharSequence drawerTitle;
+    private CharSequence title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        drawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item,
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        drawerList.setAdapter(new ArrayAdapter<>(
+                this,
+                R.layout.drawer_list_item,
                 menuOptions));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        title = drawerTitle = getTitle();
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open,
+                R.string.drawer_close) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                getSupportActionBar().setTitle(title);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        setTitle(title);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment = new BaseFragment();
+        Bundle args = new Bundle();
+        args.putString(BaseFragment.ARG_FRAGMENT_NAME, menuOptions[position]);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        drawerList.setItemChecked(position, true);
+        setTitle(menuOptions[position]);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.title = title;
+        getSupportActionBar().setTitle(this.title);
     }
 
     private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
@@ -40,23 +133,5 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
             selectItem(position);
         }
-
-        private void selectItem(int position) {
-            Fragment fragment = new BaseFragment();
-            Bundle args = new Bundle();
-            args.putString(BaseFragment.ARG_FRAGMENT_NAME, menuOptions[position]);
-            fragment.setArguments(args);
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame, fragment)
-                    .commit();
-
-            drawerList.setItemChecked(position, true);
-            setTitle(menuOptions[position]);
-            drawerLayout.closeDrawer(drawerList);
-        }
     }
-
-
 }
